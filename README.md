b5312f4bf0d2f0c7c0455119701533c9c0a66031
Pedro Landolt <pedrolandolt88@gmail.com>
2023-04-28 10:53:42 +0100

    LeicPlan Done!

---
+++ README.md
@@ -0,0 +1,3 @@
+### Project 1 - Leic Student Schedule Management, LeicPlan (Grade: 18.55 / 20)
+
+A project done in one mounth with [João Coelho](https://github.com/Jcoelho13) and [João Mota](https://github.com/Joao-Mota). <br>
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/motaj/Desktop/Faculdade/Discplinas/AED/aed_projeto
+# Install script for directory: C:/Users/pland/OneDrive/Desktop/Update_Git/AED/LeicPlan
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2022.2.3/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Users/pland/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/231.8109.222/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/motaj/Desktop/Faculdade/Discplinas/AED/aed_projeto/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/pland/OneDrive/Desktop/Update_Git/AED/LeicPlan/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
